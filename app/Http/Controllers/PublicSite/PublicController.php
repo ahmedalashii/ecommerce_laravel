a192aeadc73a8e49fb602e868af71cc446ef729b
ahmed_alashii <you@example.com>
2022-11-30 20:45:32 +0200

    Changes ..

--- app/Http/Controllers/PublicSite/PublicController.php
+++ app/Http/Controllers/PublicSite/PublicController.php
@@ -10,6 +10,7 @@ class PublicController extends Controller
 {
     public function index()
     {
+        // $stores = Store::join('products', 'products.store_id', 'stores.id')->selectRaw('products.store_id')->groupBy('products.store_id')->orderByRaw('COUNT(products.store_id) DESC')->take(4)->get();
         $stores = Store::with('products')->take(4)->get();
         return view('public_site.index')->with('stores', $stores);
     }
